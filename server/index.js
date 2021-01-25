027e468574faef2d09f559c6c5cf0056e83c1bc6
LordBigglesworth <javanpohl@gmail.com>
2021-01-25 11:25:55 -0700

    Updating server branch

--- server/index.js
+++ server/index.js
@@ -1,39 +1,30 @@
 const express = require('express');
 const path = require('path');
-const config = require('../webpack.config.js');
-const db = require('../database/getProps.js');
-
-//middleware
-const morgan = require('morgan');
-const parser = require('body-parser');
-
-//router
-// const router = require('./routes.js');
-// const mongoRouter = require('./mongoRoutes.js');
-
-const PORT = 3000;
+const cors = require('cors');
 
 const app = express();
+const PORT = 3000;
+
+// middleware
+const morgan = require('morgan');
+const parser = require('body-parser');
 
 // logging and parsing
 app.use(morgan('dev'));
 app.use(parser.json());
-app.use(parser.urlencoded({extended: false}));
+app.use(parser.urlencoded({ extended: true }));
 
-// set-up routes
-// app.use('/api', router);
-// app.use('/api', mongoRouter);
+// database
+const db = require('../database/getProps.js');
 
 // serve the client files (webpage)
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/api/props/:propId', (req, res) => {
-  console.log('GET request in process...');
-  // res.send('Hello from the server!');
-  // db.getAll(req, res);
+app.get('/api/props/:propId', cors(), (req, res) => {
   db.getById(req, res);
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server listening at localhost:${PORT}!`);
 });
